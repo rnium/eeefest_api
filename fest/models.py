23e1b5a33acd1f9ab73ac427376ff9681d9019c1
rony <rniumo@gmail.com>
2024-02-24 00:05:57 +0600

    phone number max_length fixed

--- fest/models.py
+++ fest/models.py
@@ -35,7 +35,7 @@ class GroupMember(models.Model):
     reg_num = models.CharField(max_length=200, null=True, blank=True)
     dept = models.CharField(max_length=100, null=True, blank=True)
     tshirt = models.CharField(max_length=5, null=True, blank=True)
-    phone = models.CharField(max_length=5, null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
     other_info = models.TextField(null=True, blank=True)
     
