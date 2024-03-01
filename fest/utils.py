bf0bf6837e8bca98a9926dfbad8c0a604eff6700
rony <rniumo@gmail.com>
2024-03-02 02:31:06 +0600

    excel

--- fest/utils.py
+++ fest/utils.py
@@ -22,14 +22,14 @@ def get_registrations_queryset(contest, approval):
         registrations = registrations.order_by('is_approved', '-added_at')
     return registrations
 
-def get_lfr_or_poster_excel_data(contest, approval):
+def get_group_type_excel_data(contest, approval):
     registrations = list(get_registrations_queryset(contest, approval))
     data = []
     header = ["SL", 'reg_date', 'contest', 'approval status', 'Team Name', 'Total Members', 'Team Leader']
     max_members = 1
     for index, reg in enumerate(registrations):
         unit_data = [index+1]
-        unit_data.append(reg.added_at)
+        unit_data.append(reg.added_at.strftime("%Y-%m-%d %H:%M:%S"))
         unit_data.append(reg.contest)
         unit_data.append("Approved" if reg.is_approved else "Pending")
         unit_data.append(reg.team_name)
@@ -38,15 +38,49 @@ def get_lfr_or_poster_excel_data(contest, approval):
         max_members = max(max_members, group_members_qs.count())
         for member in group_members_qs:
             info = member.name.title()
-            info += f"\n{member.inst}"
+            info += f" ({member.inst},"
             if reg_num:=member.reg_num:
-                info += f"\nRegistration No.{reg_num}"
+                info += f" Registration No.{reg_num},"
             if dept:=member.dept:
-                info += f"\nDepartment: {dept}"
+                info += f" Department: {dept},"
             if tshirt:=member.tshirt:
-                info += f"\Tshirt: {tshirt.upper()}"
+                info += f" Tshirt: {tshirt.upper()},"
             if phone:=member.phone:
-                info += f"\Phone: {phone}"
+                info += f" Phone: {phone},"
             if email:=member.email:
-                info += f"\Email: {email.lower()}"
+                info += f" Email: {email.lower()})"
+            unit_data.append(info)
+        data.append(unit_data)
+    header.extend([f"Group Member {i+1}" for i in range(1, max_members)])
+    data.insert(0, header)
+    return data
+
+
+def get_individual_type_excel_data(contest, approval):
+    registrations = list(get_registrations_queryset(contest, approval))
+    data = []
+    header = ["SL", 'reg_date', 'contest', 'approval status', 'Contestant Name']
+    for index, reg in enumerate(registrations):
+        unit_data = [index+1]
+        unit_data.append(reg.added_at)
+        unit_data.append(reg.contest)
+        unit_data.append("Approved" if reg.is_approved else "Pending")
+        unit_data.append(reg.team_name)
+        unit_data.append(reg.group_members_count)
+        member = reg.groupmember_set.all().first()
+        info = member.name.title()
+        info += f" ({member.inst},"
+        if reg_num:=member.reg_num:
+            info += f" Registration No.{reg_num},"
+        if dept:=member.dept:
+            info += f" Department: {dept},"
+        if phone:=member.phone:
+            info += f" Phone: {phone},"
+        if email:=member.email:
+            info += f" Email: {email.lower()})"
+        unit_data.append(info)
+        data.append(unit_data)
+    data.insert(0, header)
+    return data
         
+      
--- fest/views.py
+++ fest/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import FileResponse, HttpResponse
 from django.conf import settings
+from django.core.files.base import ContentFile
 import os
+import time
+from io import BytesIO
+from openpyxl import Workbook
+from datetime import datetime
+from openpyxl.styles import Alignment
+from .utils import get_group_type_excel_data, get_individual_type_excel_data
 
 
 
@@ -11,3 +18,35 @@ def download_asset(request, filename):
     if os.path.exists(filepath):
         return FileResponse(open(filepath, 'rb'), filename=filename)
     return HttpResponse(f"File not found!")
+
+
+def download_response_excel(request):
+    contest = request.GET.get('contest', 'all')
+    approval = request.GET.get('approval', 'all')
+    if contest in ['all', 'poster', 'lfr']:
+        data = get_group_type_excel_data(contest, approval)
+    else:
+        data = get_individual_type_excel_data(contest, approval)
+    workbook = Workbook()
+    worksheet = workbook.active
+    for row_index, row_data in enumerate(data):
+        for column_index, cell_value in enumerate(row_data):
+            worksheet.cell(row=row_index + 1, column=column_index + 1, value=cell_value)
+            
+    # Stylings
+    num_cols = worksheet.max_column
+    for i in range(num_cols):
+        worksheet.column_dimensions[chr(ord('A')+i)].width = 20
+        if i > 25:
+            break
+    for row in worksheet.iter_rows():
+        for cell in row:
+            cell.alignment = Alignment(horizontal='center', vertical='center')
+    buffer = BytesIO()
+    workbook.save(buffer)
+    filename = f'Response {contest.upper()} {datetime.now().strftime("%H:%M:%S %Y-%m-%d")}.xlsx'
+    return FileResponse(
+        ContentFile(buffer.getvalue()),
+        content_type='application/vnd.ms-excel', 
+        filename=filename, as_attachment=True
+    )
